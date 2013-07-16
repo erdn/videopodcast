58fa517122911c783bf9ff2403bd82aa654ec2ba
erdn <nacholg@gmail.com>
2013-07-16 02:55:06 +0200

    pantalla maquetada

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
-
+/**
+* Funcion a la que pasamos una url, carga el video, y cuando esta listo
+* oculta distractor y reproduce el video
+*
+* @url String con url al video
+*/
+function setVideo(url){
+ video.src=url;
+ video.load();
+ video.onload=function(){
+  console.log("cargado");
+  video.play();
+ }();
+}
