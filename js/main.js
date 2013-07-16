541f152d8c57c1fbc3440ef3206947b57b7a62f9
nacho <nacho@MBNacho>
2013-07-16 14:17:58 +0200

    primer scroll + video funcionando

--- js/main.js
+++ js/main.js
@@ -1,105 +1,289 @@
 /**
-* Funcion llamada cuando DOM ha terminado de cargar
-*/
+ * Funcion llamada cuando DOM ha terminado de cargar
+ */
 $(document).ready(function() {
-	var pos = 0,
-		item = 0,
-		itemSelected =0,
-		items = 13;
+    /* variables  */
+    var videoPodcast = 'http://127.0.0.1/services/podcasting/ac360/rss.xml',
+    	pos = 0,
+        item = 0,
+        itemSelected = 0,
+        items = 13,
+     	container = $('#ul-container'),
+        lista = $('#episode-list'),
+        description = $('#video-description')
+        podcast = {};
+        
 
-	var container = $("#ul-container"),
-		lista = $("#episode-list");
-		
+    /* llamadas inicializacion */
+    
+    
+    //Asigno listener evento de teclas
+    $(document).keydown(keyDownListener);
 
-	//Asigno listener evento de teclas
-	$(document).keydown(function(event){
-		console.log(event.keyCode);
-		
-		switch(event.keyCode){
-			case 38:
-			// dpad up
-			if(0===pos && 0 < item){
-				toggleFocus(item);
-				item--;				
-				scroll(item);
-				toggleFocus(item);
-			}else if(0!==pos){
-				toggleFocus(item);
-				item--;
-				pos--;
-				toggleFocus(item);
-			}
+    //recupero el rss
+    fetchRSS(videoPodcast);
+    
+    /* funciones privadas */
+    
+    /**
+    * Listener pulsaciones teclado
+    */
+    function keyDownListener(event){
 
-			break;
-			case 40:
-			// dpad down
-			if(3===pos && item < items -1 ){
-				toggleFocus(item);
-				item++;				
-				scroll(item-3);
-				toggleFocus(item);
-			}else if(3!==pos && item < items -1 ){
-				toggleFocus(item);
-				item++;
-				pos++;
-				toggleFocus(item);
-			}
-			break;
-			case 13:
-			// enter
-			toggleSelect(itemSelected);
-			itemSelected=item;
-			toggleSelect(itemSelected);
-			//TODO con el item en el que estamos
-			// extraemos los datos del objeto js y pintamos
-			// donde corresponde
-			break;
-		}
+        console.log(event.keyCode);
 
-	});
+        switch (event.keyCode) {
+            case 38:
+                // dpad up
+                dpadUp();
+                break;
 
-	/**
-	* Funcion que hace scroll sobre elemento
-	*
-	* @pos Integer Elemento sobre el que queremos que el scroll
-	* se desplace
-	*/
-	function scroll(pos){
-		//magicnumber height + 2 px de bordes
-		container.scrollTop(111*pos);
-	}
+            case 40:
+                // dpad down
+                dpadDown();
+                break;
+            case 13:
+                // enter
+                dpadEnter();
+                break;
+        }
 
-	/**
-	* Funcion que pone y quita foco 
-	*
-	* @pos Integer Elemento sobre el que queremos modificar
-	* el foco
-	*/
-	function toggleFocus(pos){
-		lista.children().eq(pos).toggleClass("focused");		
-	}
+    }
+    
+    /**
+    * Pulsada tecla dpad up
+    */
+    function dpadUp() {
+        if (0 === pos && item > 0) {
+            toggleFocus(item);
+            item--;
+            scroll(item);
+            toggleFocus(item);
+        } else if (0 !== pos) {
+            toggleFocus(item);
+            item--;
+            pos--;
+            toggleFocus(item);
+        }
+    }
 
-	/**
-	* Funcion que pone y quita borde seleccion
-	*
-	* @pos Integer Elemento sobre el que queremos modificar
-	* el foco
-	*/
-	function toggleSelect(pos){
-		lista.children().eq(pos).toggleClass("selected");		
-	}
+    /**
+    * Pulsada tecla dpad down
+    */
+
+    function dpadDown() {
+        if (3 === pos && item < items - 1) {
+            toggleFocus(item);
+            item++;
+            scroll(item - 3);
+            toggleFocus(item);
+        } else if (3 !== pos && item < items - 1) {
+            toggleFocus(item);
+            item++;
+            pos++;
+            toggleFocus(item);
+        }
+    }
+
+    /**
+     * Pulsada tecla dpad enter
+     */
+    function dpadEnter() {
+        toggleSelect(itemSelected);
+        itemSelected = item;
+        toggleSelect(itemSelected);
+
+        //pongo descriptcion y video
+        description.html(podcast.items[item].description);
+        setVideo(podcast.items[item].media);        
+    }
+
+    /**
+    * Metodo que obtiene el rss y genera un objeto JS
+    * con el mismo
+    */
+    function fetchRSS(url){
+        var xhr = new XMLHttpRequest(),
+            namespaces = {};
+
+        
+        //ponemos ruta y enviamos peticon
+        xhr.open('GET', url);
+        xhr.onreadystatechange = stateChange;
+        xhr.send();
+    }
+
+    /**
+    * Funcion llamada cuando cambia el estado de la conexion
+    * async
+    */
+    function stateChange() {
+        if (4 === this.readyState && 200 === this.status) {
+            var item = {},              
+                xml = this.responseXML,
+                attr,
+                xmlItems,
+                i;
+
+            //extraemos los posibles ns
+            namespaces = getNameSpaces(xml);
+            
+            //extramos los valores del videopodcast
+            podcast.title = getTagNameValue("title", xml);
+            podcast.description = getTagNameValue("description", xml);
+            
+            //creamos array para los objetos de item
+            podcast.items = [];
+
+            xmlItems = xml.getElementsByTagName("item");
+
+            for (i = 0; i < xmlItems.length; i++) {
+                //inicializamos objeto item
+                item = {}
+
+                //extraemos los valores y atributos de tags
+                item.title = getTagNameValue("title", xmlItems[i]);
+                item.pubDate = getTagNameValue("pubDate", xmlItems[i]);
+                item.description = getTagNameValue("description", xmlItems[i]);
+                item.media = getTagNameAttribute("media:content","url",xmlItems[i]);
+                
+                //encolamos en array
+                podcast.items.push(item);
+            }
+
+            //TODO inicializamos valores, renderizamos la vista, ponemos foco donde 
+            //corresponde, no reproducimos video
+        }
+    }
+
+    /**
+    * Funcion que mapea xmlns:ns con su url
+    * para poder buscar tags de forma simplificada
+    * @xml documento xml de donde se extraeran los xmlns
+    *
+    * @return namespaces objeto key(ns) value (url ns)
+    */
+    function getNameSpaces(xml){
+        var namespaces = {},
+            attr = xml.documentElement.attributes;
+        
+        for (i=0;i<attr.length;i++){
+            namespaces[attr[i].name.split(":")[1]]=attr[i].value;               
+        }
+
+        return namespaces;
+    }
+
+    /**
+     * @tag tag del que queremos extraer su valor
+     * @xml
+     *
+     * @return String Valor del tag o "" si error
+     */
+    function getTagNameValue(tag, xml) {
+        var aux,
+            ns,
+            retorno;
+        
+        try{
+            //extraemos tag y ns        
+            aux = tag.split(":");
+            
+            if(aux.length>1){
+                //hay mas de 1 elemento, ns
+                ns=namespaces[aux[0]];
+                retorno = xml.getElementsByTagNameNS(ns,aux[1])[0].textContent.toString();
+            }else{
+                retorno = xml.getElementsByTagName(tag)[0].textContent.toString();
+            }
+        }catch(e){
+            console.log("error recuperando valor "+e);
+            retorno = "";
+        }
+
+        return retorno;
+    }
+
+
+    /**
+     *
+     *
+     * @tag String Tag del que queremos extraer su valor
+     * @attribute String atributo que queremos recuperar
+     * @xml 
+     *
+     * @return String Valor del tag o "" si error
+     */
+    function getTagNameAttribute(tag,attribute, xml) {
+        var aux,
+            ns,
+            retorno;
+        
+        try{
+            aux = tag.split(":");
+            
+            if(aux.length>1){
+                ns=namespaces[aux[0]];
+                retorno = xml.getElementsByTagNameNS(ns,aux[1])[0].getAttribute(attribute);
+            }else{
+                retorno = xml.getElementsByTagName(tag)[0].getAttribute(attribute);
+            }
+        }catch(e){
+            console.log("error recuperado atributo "+e);
+            retorno = "";
+        }
+        
+        return retorno;
+    }
+
+    /**
+     * Funcion que hace scroll sobre elemento
+     *
+     * @pos Integer Elemento sobre el que queremos que el scroll
+     * se desplace
+     */
+
+    function scroll(pos) {
+        //magicnumber height + 2 px de bordes
+        container.scrollTop(111 * pos);
+    }
+
+    /**
+     * Funcion que pone y quita foco
+     *
+     * @pos Integer Elemento sobre el que queremos modificar
+     * el foco
+     */
+
+    function toggleFocus(pos) {
+        lista.children().eq(pos).toggleClass("focused");
+    }
+
+    /**
+     * Funcion que pone y quita borde seleccion
+     *
+     * @pos Integer Elemento sobre el que queremos modificar
+     * el foco
+     */
+
+    function toggleSelect(pos) {
+        lista.children().eq(pos).toggleClass("selected");
+    }
+
+   
 });
 /**
-* Funcion a la que pasamos una url, carga el video, y cuando esta listo
-* oculta distractor y reproduce el video
-*
-* @url String con url al video
-*/
-function setVideo(url){
- video.src=url;
- video.load();
- video.onload=function(){
-  console.log("cargado");
-  video.play();
- }();
+ * Funcion a la que pasamos una url, carga el video, y cuando esta listo
+ * oculta distractor y reproduce el video
+ *
+ * @url String con url al video
+ */
+
+function setVideo(url) {
+    video.src = url;
+    video.load();
+    video.onload = function() {
+        console.log("cargado");
+        video.play();
+    }();
 }
