7828c29a7ffb876e6dc7cc6cfa672c7dc0341072
nacho <nacho@MBNacho>
2013-07-16 18:52:25 +0200

    limpiado codigo

--- js/main.js
+++ js/main.js
@@ -14,25 +14,26 @@ $(document).ready(function() {
         podcast = {},
         xhr = new XMLHttpRequest(),
         namespaces = {};
-        
+
 
     /* llamadas inicializacion */
-    
-    
+
     //Asigno listener evento de teclas
     $(document).keydown(keyDownListener);
 
     //recupero el rss
     fetchRSS(videoPodcast);
-    
+
+
     /* funciones privadas */
-    
+
     /**
-    * Listener pulsaciones teclado
-    */
-    function keyDownListener(event){
-        var keyCode = (event.keyCode ? event.keyCode : event.which);
-        console.log(keyCode);
+     * Listener pulsaciones teclado
+     */
+
+    function keyDownListener(event) {
+        //compatibilidad entre browsers
+        var keyCode = (event.keyCode ? event.keyCode : event.which);     
 
         switch (keyCode) {
             case 38:
@@ -44,17 +45,19 @@ $(document).ready(function() {
                 // dpad down
                 dpadDown();
                 break;
+
             case 13:
                 // enter
                 dpadEnter();
                 break;
         }
-
     }
-    
+
+
     /**
-    * Pulsada tecla dpad up
-    */
+     * Pulsada tecla dpad up
+     */
+
     function dpadUp() {
         if (0 === pos && itemPos > 0) {
             toggleFocus(itemPos);
@@ -69,9 +72,10 @@ $(document).ready(function() {
         }
     }
 
+
     /**
-    * Pulsada tecla dpad down
-    */
+     * Pulsada tecla dpad down
+     */
 
     function dpadDown() {
         if (3 === pos && itemPos < itemNum - 1) {
@@ -87,9 +91,11 @@ $(document).ready(function() {
         }
     }
 
+
     /**
      * Pulsada tecla dpad enter
      */
+
     function dpadEnter() {
         toggleSelect(itemSel);
         itemSel = itemPos;
@@ -97,28 +103,32 @@ $(document).ready(function() {
 
         //pongo descriptcion y video
         description.html(podcast.items[itemPos].description);
-        setVideo(podcast.items[itemPos].media);        
+        setVideo(podcast.items[itemPos].media);
     }
 
+
     /**
-    * Metodo que obtiene el rss y genera un objeto JS
-    * con el mismo
-    */
-    function fetchRSS(url){
-               
+     * Metodo que obtiene el rss y genera un objeto JS
+     * con el mismo
+     */
+
+    function fetchRSS(url) {
+
         //ponemos ruta y enviamos peticon
         xhr.open('GET', url);
         xhr.onreadystatechange = stateChange;
         xhr.send();
     }
 
+
     /**
-    * Funcion llamada cuando cambia el estado de la conexion
-    * async
-    */
+     * Funcion llamada cuando cambia el estado de la conexion
+     * async
+     */
+
     function stateChange() {
         if (4 === this.readyState && 200 === this.status) {
-            var item = {},              
+            var item = {},
                 xml = this.responseXML,
                 attr,
                 xmlItems,
@@ -126,11 +136,11 @@ $(document).ready(function() {
 
             //extraemos los posibles ns
             namespaces = getNameSpaces(xml);
-            
+
             //extramos los valores del videopodcast
             podcast.title = getTagNameValue("title", xml);
             podcast.description = getTagNameValue("description", xml);
-            
+
             //creamos array para los objetos de item
             podcast.items = [];
 
@@ -144,8 +154,8 @@ $(document).ready(function() {
                 item.title = getTagNameValue("title", xmlItems[i]);
                 item.pubDate = getTagNameValue("pubDate", xmlItems[i]);
                 item.description = getTagNameValue("description", xmlItems[i]);
-                item.media = getTagNameAttribute("media:content","url",xmlItems[i]);
-                
+                item.media = getTagNameAttribute("media:content", "url", xmlItems[i]);
+
                 //encolamos en array
                 podcast.items.push(item);
             }
@@ -159,29 +169,32 @@ $(document).ready(function() {
 
             // lista de videos, descripcion selecion y foco al primer elemento            
             $("#episode-list").html(Handlebars.templates.item(podcast));
-            description.html(podcast.items[itemPos].description);                       
+            description.html(podcast.items[itemPos].description);
             toggleFocus(itemPos);
             toggleSelect(itemSel);
         }
     }
 
+
     /**
-    * Funcion que mapea xmlns:ns con su url
-    * para poder buscar tags de forma simplificada
-    * @xml documento xml de donde se extraeran los xmlns
-    *
-    * @return namespaces objeto key(ns) value (url ns)
-    */
-    function getNameSpaces(xml){
+     * Funcion que mapea xmlns:ns con su url
+     * para poder buscar tags de forma simplificada
+     * @xml documento xml de donde se extraeran los xmlns
+     *
+     * @return namespaces objeto key(ns) value (url ns)
+     */
+
+    function getNameSpaces(xml) {
         var namespaces = {},
             attr = xml.documentElement.attributes;
-        
-        for (i=0;i<attr.length;i++){
-            namespaces[attr[i].name.split(":")[1]]=attr[i].value;               
+
+        for (i = 0; i < attr.length; i++) {
+            namespaces[attr[i].name.split(":")[1]] = attr[i].value;
         }
 
         return namespaces;
     }
+
 
     /**
      * @tag tag del que queremos extraer su valor
@@ -189,24 +202,25 @@ $(document).ready(function() {
      *
      * @return String Valor del tag o "" si error
      */
+
     function getTagNameValue(tag, xml) {
         var aux,
             ns,
             retorno;
-        
-        try{
+
+        try {
             //extraemos tag y ns        
             aux = tag.split(":");
-            
-            if(aux.length>1){
+
+            if (aux.length > 1) {
                 //hay mas de 1 elemento, ns
-                ns=namespaces[aux[0]];
-                retorno = xml.getElementsByTagNameNS(ns,aux[1])[0].textContent.toString();
-            }else{
+                ns = namespaces[aux[0]];
+                retorno = xml.getElementsByTagNameNS(ns, aux[1])[0].textContent.toString();
+            } else {
                 retorno = xml.getElementsByTagName(tag)[0].textContent.toString();
             }
-        }catch(e){
-            console.log("error recuperando valor "+e);
+        } catch (e) {
+            console.log("error recuperando valor " + e);
             retorno = "";
         }
 
@@ -219,31 +233,33 @@ $(document).ready(function() {
      *
      * @tag String Tag del que queremos extraer su valor
      * @attribute String atributo que queremos recuperar
-     * @xml 
+     * @xml
      *
      * @return String Valor del tag o "" si error
      */
-    function getTagNameAttribute(tag,attribute, xml) {
+
+    function getTagNameAttribute(tag, attribute, xml) {
         var aux,
             ns,
             retorno;
-        
-        try{
+
+        try {
             aux = tag.split(":");
-            
-            if(aux.length>1){
-                ns=namespaces[aux[0]];
-                retorno = xml.getElementsByTagNameNS(ns,aux[1])[0].getAttribute(attribute);
-            }else{
+
+            if (aux.length > 1) {
+                ns = namespaces[aux[0]];
+                retorno = xml.getElementsByTagNameNS(ns, aux[1])[0].getAttribute(attribute);
+            } else {
                 retorno = xml.getElementsByTagName(tag)[0].getAttribute(attribute);
             }
-        }catch(e){
-            console.log("error recuperado atributo "+e);
+        } catch (e) {
+            console.log("error recuperado atributo " + e);
             retorno = "";
         }
-        
+
         return retorno;
     }
+
 
     /**
      * Funcion que hace scroll sobre elemento
@@ -257,6 +273,7 @@ $(document).ready(function() {
         container.scrollTop(111 * pos);
     }
 
+
     /**
      * Funcion que pone y quita foco
      *
@@ -267,6 +284,7 @@ $(document).ready(function() {
     function toggleFocus(pos) {
         lista.children().eq(pos).toggleClass("focused");
     }
+
 
     /**
      * Funcion que pone y quita borde seleccion
@@ -279,20 +297,19 @@ $(document).ready(function() {
         lista.children().eq(pos).toggleClass("selected");
     }
 
-   
-});
-/**
- * Funcion a la que pasamos una url, carga el video, y cuando esta listo
- * oculta distractor y reproduce el video
- *
- * @url String con url al video
- */
 
-function setVideo(url) {
-    video.src = url;
-    video.load();
-    video.onload = function() {
-        console.log("cargado");
-        video.play();
-    }();
-}
+    /**
+     * Funcion a la que pasamos una url, carga el video, y cuando esta listo
+     * oculta distractor y reproduce el video
+     *
+     * @url String con url al video
+     */
+
+    function setVideo(url) {
+        video.src = url;
+        video.load();
+        video.onload = function() {            
+            video.play();
+        }();
+    }
+});
